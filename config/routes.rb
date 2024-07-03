c0fae8c75f6174481d6f92ec8fa6914d2606e23e
young <hyangyoungs@gmail.com>
2024-07-03 14:45:08 -0500

    update: add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  resources :likes
-  resources :follow_requests
-  resources :comments
-  devise_for :users
-  resources :photos
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "photos#index"
+
+  devise_for :users
+
+  resources :comments
+  resources :follow_requests
+  resources :likes
+  resources :photos
 end
